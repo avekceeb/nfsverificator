34d58202510c53e91fd47c0b3614a8011e5368ea
Yup Its Me <me@here.com>
2018-11-06 18:18:09 +0300

    v4.0 look

--- tests/v41/v41_functional_test.go
+++ tests/v41/v41_functional_test.go
@@ -7,22 +7,63 @@ import (
 	"time"
 )
 
+var (
+	c *NFSv41Client
+)
+
 var _ = Describe("Functional", func() {
 
-	//BeforeSuite(func() {
-	//})
-	//
-	//AfterSuite(func() {
-	//})
+	BeforeSuite(func() {
+		c = NewNFSv41Client(
+			Config.ServerHost, Config.ServerPort,
+				RandString(8) + ".fake.net", 0, 0, RandString(8))
+		c.ExchangeId()
+		c.CreateSession()
+		c.Seq++
+	})
+
+	AfterSuite(func() {
+		c.Close()
+	})
 
 	Context("Basic", func() {
 
+		It("PyNFS::LOOK1", func(){
+			c.Fail(
+				NFS4ERR_NOFILEHANDLE,
+				Sequence(c.Sid, c.Seq, 0, 0, false),
+				Lookup(RandString(12)))
+		})
+
+		It("PyNFS::LOOK2", func(){
+			c.Seq++
+			c.Fail(
+				NFS4ERR_NOENT,
+				Sequence(c.Sid, c.Seq, 0, 0, false),
+				Putrootfh(),
+				Lookup(RandString(12)))
+		})
+
+		It("PyNFS::LOOK3", func(){
+			c.Seq++
+			c.Fail(
+				NFS4ERR_INVAL,
+				Sequence(c.Sid, c.Seq, 0, 0, false),
+				Putrootfh(),
+				Lookup(""))
+		})
+
+		It("PyNFS::LOOK4", func(){
+			c.Seq++
+			c.Fail(
+				NFS4ERR_NAMETOOLONG,
+				Sequence(c.Sid, c.Seq, 0, 0, false),
+				Putrootfh(),
+				Lookup(RandString(4000)))
+		})
+
 		It("PyNFS::EID9", func() {
-			c := NewNFSv41Client(
-				Config.ServerHost, Config.ServerPort,
-					RandString(8) + ".fake.net", 0, 0, RandString(8))
-			c.ExchangeId()
-			c.CreateSession()
+			Skip("just too long")
 			c.Seq++
 			l := c.Pass(
 				Sequence(c.Sid, c.Seq, 0, 0, false),
